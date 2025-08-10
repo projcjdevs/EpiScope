d30bc62c28140c90636ff6d147352f19eccfb90e
renzv-compsci <viloriarenz917@gmail.com>
2025-08-11 00:02:52 +0800

    Add cors middleware

--- dataScrape/api_server.py
+++ dataScrape/api_server.py
@@ -5,6 +5,17 @@ from fastapi import FastAPI, HTTPException, Header
 from pydantic import BaseModel
 
 app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or ["http://localhost:3000"] for stricter security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 API_KEY = "clean-data-api"
 
 def load_data():
